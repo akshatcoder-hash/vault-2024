0e787a1db224d0a243e220178093b40849d6293c
akshatcoder-hash <akshatcoder@outlook.com>
2024-02-28 21:00:47 +0530

    get started with creating and deposit functions

--- programs/vault-2024/src/lib.rs
+++ programs/vault-2024/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_lang::system_program::{transfer, Transfer};
 
 declare_id!("73kVpBT3u7awKf4N7aW3QqUbbDgb6PKvHN7SgD7YwSXf");
 
@@ -6,10 +7,53 @@ declare_id!("73kVpBT3u7awKf4N7aW3QqUbbDgb6PKvHN7SgD7YwSXf");
 pub mod vault_2024 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
+    pub fn deposit(ctx: Context<Deposit>, seed: u64, lamports: u64) -> Result<()> {
+        ctx.accounts.vault.set_inner(Vault {
+            maker: ctx.accounts.maker.key(),
+            taker: ctx.accounts.taker.key(),
+            seed,
+            bump: ctx.bumps.vault,
+            created_at: Clock::get()?.unix_timestamp,
+        });
+        let accts = Transfer {
+            from: ctx.accounts.maker.to_account_info(),
+            to: ctx.accounts.vault.to_account_info(),
+        };
+        let cpi_ctx = CpiContext::new(ctx.accounts.system_program.to_account_info(), accts);
+        transfer(cpi_ctx, lamports)
+    }
+
+    pub fn cancel(ctx: Context<Cancel>) -> Result<()> {
+        unimplemented!()
+    }
+
+    pub fn claim(ctx: Context<Cancel>) -> Result<()> {
+        unimplemented!()
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(seed:u64)]
+pub struct Deposit<'info> {
+    #[account(mut)]
+    pub maker: Signer<'info>,
+    /// CHECK: this is ok.
+    pub taker: UncheckedAccount<'info>,
+    #[account(init, payer=maker, seeds = [b"vault", seed.to_le_bytes().as_ref(),  maker.key().as_ref(), taker.key().as_ref()], bump, space=Vault::INIT_SPACE)]
+    pub vault: Account<'info, Vault>,
+    pub system_program: Program<'info, System>,
+}
+
+impl Space for Vault {
+    const INIT_SPACE: usize = 8 + 32 + 32 + 8 + 1 + 8;
+}
+
+//8 + 32
+#[account]
+pub struct Vault {
+    pub maker: Pubkey,
+    pub taker: Pubkey,
+    pub seed: u64,
+    pub bump: u8,
+    pub created_at: i64,
+}
